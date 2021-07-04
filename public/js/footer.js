cda5bad8dc2127b9e358afa8f88eba9a594c3246
Malcolm Kee <malcolm.keeweesiong@gmail.com>
2021-07-04 21:36:56 +0800

    demo update UI using innerHTML every 1 sec

--- public/js/footer.js
+++ public/js/footer.js
@@ -8,10 +8,3 @@ const footerYearEl = document.querySelector('#footer-year');
 // update the content of the year element to result of helpers.getCurrentYear
 footerYearEl.innerHTML = helpers.getCurrentYear();
 
-console.log('getCurrentDate', helpers.getCurrentDate());
-/* EXERCISE: practice how to update current date */
-
-// TODO: get the footer today element
-const footerTodayEl = document.querySelector('#footer-today');
-// TODO: update the content of the today element to result of helpers.getCurrentDate
-footerTodayEl.innerHTML = helpers.getCurrentDate();
